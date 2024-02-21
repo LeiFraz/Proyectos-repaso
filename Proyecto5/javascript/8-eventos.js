0e5b58633f13ff26914ffbd134d30740efcd14ea
Leonardo Paz <flajarkin99@hotmail.com>
2024-02-21 17:56:35 -0300

    8-eventos.js y html, temas: StopPropagation y PreventDefault (para evitar el burbujeo o propagación), Delegación de eventos mediante ID, MATCHES y DATASET

---
+++ Proyecto5/javascript/8-eventos.js
@@ -0,0 +1,42 @@
+const cajitas = document.querySelectorAll('.border');
+
+/* STOP PROPAGATION = evita que los demas eventos se activen, frena la propagación del evento */
+// cajitas.forEach((caja) => caja.addEventListener("click", (e) => {
+    
+//     e.stopPropagation();
+//     console.log("me diste click y use STOP PROPAGATION")
+//     }
+//     ));
+
+const formulario = document.querySelector('form');
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("me hiciste click y use PREVENT DEFAULT")
+})
+
+/* Delegación de Eventos */
+
+const contenedor = document.querySelector('.container');
+
+contenedor.addEventListener("click", (e) =>
+{
+    //Delegacion de eventos con ID
+    if(e.target.id === "padre"){
+        console.log("hiciste click al padre con el ID")
+    }
+
+    //Delegacion de eventos con MATCHES
+    if(e.target.matches(".border-secondary"))
+    {
+        console.log("hiciste click al hijo con MATCHES")
+    }
+
+    //Delegacion de eventos con DATASET
+    
+    if(e.target.dataset.div === "divNieto")
+    {
+        console.log("hiciste click al nieto con DATASET")
+    }
+
+})
